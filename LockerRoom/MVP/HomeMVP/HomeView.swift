b182427678f4fac1844f8b176e0d335cd7e96f0b
Ognjen22 <pavlovic.ognjen51@gmail.com>
2022-11-07 08:35:20 +0100

    removed hardcoded array members from popup Button

--- LockerRoom/MVP/HomeMVP/HomeView.swift
+++ LockerRoom/MVP/HomeMVP/HomeView.swift
@@ -32,64 +32,27 @@ class HomeView: UIView {
     
     func setup(with player: PlayerModel) {
         
-        let optionClosure = {(action: UIAction) in
-            if action.title == "KRK"
-            {
-                self.ppgLabel.text = "PTS \n" + player.data.leagues[1].stats.PTS
-                self.rpgLabel.text = "RPG \n" + player.data.leagues[1].stats.REB
-                self.apgLabel.text = "APG \n" + player.data.leagues[1].stats.AST
-                self.bpgLabel.text = "BPG \n" + player.data.leagues[1].stats.BLK
-                self.tpmLabel.text = "TPM \n" + player.data.leagues[1].stats.ThreePM
-                self.fpgLabel.text = "FPB \n" + player.data.leagues[1].stats.FGpercent
-                self.tppLabel.text = "TPP \n" + player.data.leagues[1].stats.ThreePercent
-                self.spgLabel.text = "SPG \n" + player.data.leagues[1].stats.STL
+        let optionClosure = { (action: UIAction) in
+            
+            let options = self.popUpButtonStates.menu?.children ?? []
+            
+            for (index, option) in options.enumerated() where (option as? UIAction) == action {
+                self.setupStatsData(league: player.data.leagues[index])
             }
-            else if action.title == "1 MRLC A" {
-                self.ppgLabel.text = "PTS \n" + player.data.leagues[0].stats.PTS
-                self.rpgLabel.text = "RPG \n" + player.data.leagues[0].stats.REB
-                self.apgLabel.text = "APG \n" + player.data.leagues[0].stats.AST
-                self.bpgLabel.text = "BPG \n" + player.data.leagues[0].stats.BLK
-                self.tpmLabel.text = "TPM \n" + player.data.leagues[0].stats.ThreePM
-                self.fpgLabel.text = "FPB \n" + player.data.leagues[0].stats.FGpercent
-                self.tppLabel.text = "TPP \n" + player.data.leagues[0].stats.ThreePercent
-                self.spgLabel.text = "SPG \n" + player.data.leagues[0].stats.STL
-            }
-            else if action.title == "MRLC PLAYOFF" {
-                self.ppgLabel.text = "PTS \n" + player.data.leagues[2].stats.PTS
-                self.rpgLabel.text = "RPG \n" + player.data.leagues[2].stats.REB
-                self.apgLabel.text = "APG \n" + player.data.leagues[2].stats.AST
-                self.bpgLabel.text = "BPG \n" + player.data.leagues[2].stats.BLK
-                self.tpmLabel.text = "TPM \n" + player.data.leagues[2].stats.ThreePM
-                self.fpgLabel.text = "FPB \n" + player.data.leagues[2].stats.FGpercent
-                self.tppLabel.text = "TPP \n" + player.data.leagues[2].stats.ThreePercent
-                self.spgLabel.text = "SPG \n" + player.data.leagues[2].stats.STL
-            }
-            else if action.title == "Average" {
-                self.ppgLabel.text = "PTS \n" + player.data.average.PTS
-                self.rpgLabel.text = "RPG \n" + player.data.average.REB
-                self.apgLabel.text = "APG \n" + player.data.average.AST
-                self.bpgLabel.text = "BPG \n" + player.data.average.BLK
-                self.tpmLabel.text = "TPM \n" + player.data.average.ThreePM
-                self.fpgLabel.text = "FPB \n" + player.data.average.FGpercent
-                self.tppLabel.text = "TPP \n" + player.data.average.ThreePercent
-                self.spgLabel.text = "SPG \n" + player.data.average.STL
-            }
-
         }
         
         var optionsArray = [UIAction]()
-        optionsArray.append(UIAction(title: "Average", state: .off, handler: optionClosure))
         for league in player.data.leagues{
             let action = UIAction(title: league.league_name, state: .off, handler: optionClosure)
             optionsArray.append(action)
         }
-        optionsArray[0].state = .on
         let optionsMenu = UIMenu(title: "", options: .displayInline, children: optionsArray)
         popUpButtonStates.menu = optionsMenu
         popUpButtonStates.changesSelectionAsPrimaryAction = true
         popUpButtonStates.showsMenuAsPrimaryAction = true
-        
-        var labels = [nameLabel, heightLabel, positionLabel, locationLabel, ageLabel]
+        (popUpButtonStates.menu?.children[0] as? UIAction)?.state = .on
+        setupStatsData(league: player.data.leagues[0])
+        let labels = [nameLabel, heightLabel, positionLabel, locationLabel, ageLabel]
         for label in labels {
             if let label = label {
                 label.textColor = .white
@@ -104,9 +67,20 @@ class HomeView: UIView {
         
         let url = URL(string: player.data.player_image)
         playerPicture.layer.cornerRadius = playerPicture.frame.size.width / 2
-//        let processor = RoundCornerImageProcessor(cornerRadius: 20)
         playerPicture.clipsToBounds = true
         playerPicture.kf.setImage(with: url)
         
+    }
+    
+    private func setupStatsData(league: Leagues) {
+        
+        self.ppgLabel.text = "PTS \n" + league.stats.PTS
+        self.rpgLabel.text = "RPG \n" + league.stats.REB
+        self.apgLabel.text = "APG \n" + league.stats.AST
+        self.bpgLabel.text = "BPG \n" + league.stats.BLK
+        self.tpmLabel.text = "TPM \n" + league.stats.ThreePM
+        self.fpgLabel.text = "FPB \n" + league.stats.FGpercent
+        self.tppLabel.text = "TPP \n" + league.stats.ThreePercent
+        self.spgLabel.text = "SPG \n" + league.stats.STL
     }
 }
